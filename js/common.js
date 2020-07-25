320f34c27c85133c3c44b3397e2dc54ce767d6c5
suzusikikata <info@suzusikikata.com>
2020-07-25 18:18:11 +0900

    “アートワークページ・ソート検索機能実装”

--- js/common.js
+++ js/common.js
@@ -56,29 +56,98 @@ $(function() {
 });
 
 
-jQuery(function($){
-  var sp = 667;  //SPのサイズを設定
-  
-  function masonry_execute() {
-    var $demo2 = $('#picture_box');    //コンテナとなる要素を指定
-  
-    if ( $('html').width() < sp ) {    //ウィンドウ幅が480px以下だった場合、masonry破棄 (無効)
-      $demo2.masonry('destroy');
-    } else {                              //ウィンドウ幅が480px以上だった場合、masonry適応
-      $demo2.imagesLoaded(function(){
-        $demo2.masonry({
-	      itemSelector: '.item',  //コンテンツを指定
-	      columnWidth: 0,    //カラム幅を設定
-	      fitWidth: true,          //コンテンツ数に合わせ親の幅を自動調整
-	      resize: true,
-	      gutter:0,
-        });
-      });
-    }
+$(function(){
+  var box = $('.js_target');//検索対象のDOMを格納する
+  var conditions = $('.js_conditions');//現在の条件の選択状況を保持するオブジェクト
+  var findConditions;//各data-typeの子要素(input)を格納する
+  var currentType;//現在のdata-typeを示す
+  var count = 0;//検索ヒット数
+  var checkcount = 0;//各data-typeのチェックボックス選択数
+  var data_check = 0;//対象項目のデータがどれだけチェック状態と一致しているか
+ 
+  var condition ={};//チェックボックスの入力状態を保持するオブジェクト
+ 
+  $('.js_denominator').text(box.length);//件数表示の分母をセット
+ 
+  for(var i = 0; i < conditions.length; i++){//ターゲットのdata-typeを参照し、メソッドとしてconditionに個別に代入する
+    currentType = conditions[i].getAttribute('data-type');
+    condition[currentType] = [];
   }
-  masonry_execute();  //masonry_execute関数を実行
-  
-  $(window).resize(function(){  //ウィンドウがリサイズされたら、再度masonry_execute関数を実行
-    masonry_execute();
+ 
+  function setConditions(){//条件設定
+ 
+    count = 0;
+    box.removeClass('js_selected');
+ 
+    for(var i = 0; i < conditions.length; i++){//data-typeごとの処理
+ 
+      currentType = conditions[i].getAttribute('data-type');
+ 
+      findConditions = conditions[i].querySelectorAll('input');
+ 
+      for(var n = 0; n< findConditions.length; n++){//inputごとの処理
+ 
+        if(findConditions[n].checked){//現在選択中のインプットが選択されている場合
+          condition[currentType][findConditions[n].value] = true;
+          checkcount++
+        } else {
+          condition[currentType][findConditions[n].value] = false;
+        }
+        if(findConditions.length === n+1){//ループが最後の場合
+          if(checkcount === 0){
+            for(var t = 0; t < findConditions.length; t++){
+              condition[currentType][findConditions[t].value] = true;
+            }
+          }
+          checkcount = 0;
+        }
+      }
+    }
+ 
+ 
+    for(var m = 0, len = box.length; m< len; ++m){//最初に取得したターゲットの情報と、現在のinputの選択状態を比較して処理を行う
+ 
+      for(var i = 0; i < conditions.length; i++){//ターゲットのdata-typeを参照し、メソッドとしてconditionに個別に代入する
+        currentType = conditions[i].getAttribute('data-type');
+         
+        if(condition[currentType][$(box[m]).data(currentType)]){
+          data_check++;
+        } else {
+ 
+        }
+      }
+ 
+        if(data_check === conditions.length){
+          count++;
+          $(box[m]).addClass('js_selected');
+        }else{
+ 
+        }
+        data_check = 0;
+ 
+    }
+ 
+ 
+    $('.js_numerator').text(count);//件数表示の分子をセット
+  }
+ 
+  setConditions();
+ 
+  $(document).on('click','input',function(){
+ 
+    setConditions();
+ 
   });
+ 
+  $(document).on('click','.js_release',function(){
+ 
+    $('.art_selectBlock_check input').attr("checked", false);
+ 
+    setConditions();
+ 
+  });
+ 
+   
+ 
+ 
 });
